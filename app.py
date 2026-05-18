66e9da5cc78808f71d57eb8d1f9c8c368dd63897
Máté <csizmadiamate17@gmail.com>
2026-05-18 18:57:54 +0200

    vesszőhiba javítása

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Intelligens-Dokumentum-Elemző"
-    page_icon="🐕"
+    page_title="Intelligens-Dokumentum-Elemző",
+    page_icon="🐕",
     layout="wide"
 )
 
