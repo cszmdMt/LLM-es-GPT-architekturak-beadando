cdd9d9c7589b4277b4cdadec0a99063f5256feb8
Máté <csizmadiamate17@gmail.com>
2026-05-18 19:22:56 +0200

    pdf-ből való szöveg kinyerésére alkalmas Lite-RAG architektúra megírása

--- utils/pdf_processor.py
+++ utils/pdf_processor.py
@@ -1,7 +1,25 @@
-"""
-Utility module for processing PDF files and extracting text content.
-"""
-import PyPDF2
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def extract_text_from_pdf(file_path):
-    pass
+def extract_text_from_pdf(pdf_file):
+    text = ""
+    for pdf in pdf_file:
+        pdf_reader = PdfReader(pdf)
+
+        for page in pdf_reader.pages:
+            extracted = page.extract_text()
+            
+            if extracted:
+                text += "\n"
+
+    return text
+
+def create_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 10000,
+        chunk_overlap = 1000
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
